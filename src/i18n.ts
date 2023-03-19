102ef292e90e5a805cfffe3672ca734ac347cd1f
Aiko <aikocute@icloud.com>
2023-03-20 00:17:55 +0700

    Update Vietnamese

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,14 +4,14 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-export const i18nLanguages = ['en', 'vi-VN'];
+export const i18nLanguages = ['English', 'Vietnamese'];
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
+    fallbackLng: 'English',
     ns: ['main', 'api', 'about', 'model'],
     defaultNS: 'main',
   });
