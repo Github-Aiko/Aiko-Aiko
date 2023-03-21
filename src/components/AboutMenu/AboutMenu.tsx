3e39e2b7e0a330efde110a7fd0346f46f71cbd5c
Aiko <aikocute@icloud.com>
2023-03-21 08:41:48 +0700

    Update STK

--- src/components/AboutMenu/AboutMenu.tsx
+++ src/components/AboutMenu/AboutMenu.tsx
@@ -86,7 +86,11 @@ const AboutMenu = () => {
                   </a>
                   <div className='flex gap-x-10 gap-y-4 flex-wrap justify-center'>
                     <div className='flex flex-col items-center justify-center gap-1'>
-                      <div>{t('support.momo', { ns: 'about' })} (Aiko)</div>
+                      <div>
+                        {t('support.momo', { ns: 'about' })} (Aiko)
+                        {t('support.stkmomo', { ns: 'about' })}  
+                      </div>
+                      
                       <img
                         className='rounded-md w-32 h-32'
                         src='https://chat.aikoaiko.me/payments/momo.png'
@@ -96,6 +100,7 @@ const AboutMenu = () => {
                     <div className='flex flex-col items-center justify-center gap-1'>
                       <div>
                         {t('support.banking', { ns: 'about' })} (Aiko)
+                        {t('support.stkbanking', { ns: 'about' })}
                       </div>
                       <img
                         className='rounded-md w-32 h-32'
