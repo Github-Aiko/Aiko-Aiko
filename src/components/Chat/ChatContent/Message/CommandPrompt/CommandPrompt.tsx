960086c352c7719a682a7927ae1900558d5b8def
Aiko <aikocute@icloud.com>
2023-03-22 14:18:49 +0700

    Update CommandPrompt.tsx

--- src/components/Chat/ChatContent/Message/CommandPrompt/CommandPrompt.tsx
+++ src/components/Chat/ChatContent/Message/CommandPrompt/CommandPrompt.tsx
@@ -11,8 +11,8 @@ const CommandPrompt = ({
 }) => {
   const { t } = useTranslation();
   const prompts = useStore((state) => state.prompts);
-  const [_prompts, _setPrompts] = useState<Prompt[]>(prompts);
   const [dropDown, setDropDown] = useState<boolean>(false);
+  const [_prompts, _setPrompts] = useState<Prompt[]>(prompts);
   const [input, setInput] = useState<string>('');
 
   useEffect(() => {
@@ -27,36 +27,34 @@ const CommandPrompt = ({
     setInput('');
   }, [prompts]);
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.keyCode === 191) {
-      setDropDown(true);
-    }
-  };
-
   return (
     <div className='relative max-wd-sm'>
-      <input
-        type='text'
-        className='text-gray-800 dark:text-white p-3 text-sm border-none bg-gray-200 dark:bg-gray-600 m-0 w-full mr-0 h-8 focus:outline-none'
-        value={input}
-        placeholder={t('search') as string}
-        onChange={(e) => {
-          setInput(e.target.value);
-        }}
-        onKeyDown={handleKeyDown}
-      />
+      <button
+        className='btn btn-neutral btn-small'
+        onClick={() => setDropDown(!dropDown)}
+      >
+        /
+      </button>
       <div
         className={`${
           dropDown ? '' : 'hidden'
         } absolute top-100 bottom-100 right-0 z-10 bg-white rounded-lg shadow-xl border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group dark:bg-gray-800 opacity-90`}
       >
         <div className='text-sm px-4 py-2 w-max'>{t('promptLibrary')}</div>
+        <input
+          type='text'
+          className='text-gray-800 dark:text-white p-3 text-sm border-none bg-gray-200 dark:bg-gray-600 m-0 w-full mr-0 h-8 focus:outline-none'
+          value={input}
+          placeholder={t('search') as string}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
         <ul className='text-sm text-gray-700 dark:text-gray-200 p-0 m-0 w-max max-w-sm max-md:max-w-[90vw] max-h-32 overflow-auto'>
           {_prompts.map((cp) => (
             <li
               className='px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer text-start w-full'
               onClick={() => {
-                setInput(cp.prompt);
                 _setContent((prev) => prev + cp.prompt);
                 setDropDown(false);
               }}
