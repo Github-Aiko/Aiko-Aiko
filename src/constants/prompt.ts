8f3bd770f91073e58f60e5d36d2ba249b15d472c
Aiko <aikocute@icloud.com>
2023-03-25 09:40:37 +0700

    Update Table Prompts :D

--- src/constants/prompt.ts
+++ src/constants/prompt.ts
@@ -17,7 +17,7 @@ const defaultPrompts: Prompt[] = [
   {
     "id": "3",
     "name": "Create a Table Demo",
-    "prompt": "| Thread | Allocation | Needs |\n|--------|------------|-------|\n| T0     | 3 0 1 4    | 2 1 0 |\n| T1     | 2 2 1 0    | 1 0 1 |\n| T2     | 3 1 2 1    | 0 2 1 |\n| T3     | 0 5 1 0    | 4 1 2 |\n| T4     | 4 2 1 2    | 2 1 1 |"
+    "prompt": "| INPUT | OUTPUT |\n|--------|-------|\n| 3 0 1 4    | 2 1 0 |\n| 2 2 1 0    | 1 0 1 |\n| 3 1 2 1    | 0 2 1 |\n| 0 5 1 0    | 4 1 2 |\n| 4 2 1 2    | 2 1 1 |"
   },
 ];
 
