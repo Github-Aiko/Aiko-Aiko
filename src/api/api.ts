c07bc08e71c39489474810e2388fd21866997c65
Aiko <aikocute@icloud.com>
2023-04-03 09:37:09 +0700

    Fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -25,3 +25,54 @@ export const getChatCompletion = async (
   const data = await response.json();
   return data;
 };
+
+export const getChatCompletionStream = async (
+  endpoint: string,
+  messages: MessageInterface[],
+  config: ConfigInterface,
+  apiKey?: string
+) => {
+  const headers: HeadersInit = {
+    'Content-Type': 'application/json',
+  };
+  if (apiKey) headers.Authorization = `Bearer ${apiKey}`;
+
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      messages,
+      ...config,
+      max_tokens: null,
+      stream: true,
+    }),
+  });
+  if (response.status === 404 || response.status === 405) {
+    const text = await response.text();
+    if (text.includes('model_not_found')) {
+      throw new Error(
+        text +
+          '\nMessage from Better ChatGPT:\nPlease ensure that you have access to the GPT-4 API!'
+      );
+    } else {
+      throw new Error(
+        'Message from Better ChatGPT:\nInvalid API endpoint! We recommend you to check your free API endpoint.'
+      );
+    }
+  }
+
+  if (response.status === 429 || !response.ok) {
+    const text = await response.text();
+    let error = text;
+    if (text.includes('insufficient_quota')) {
+      error +=
+        '\nMessage from Better ChatGPT:\nWe recommend changing your API endpoint or API key';
+    } else {
+      error += '\nRate limited! Please try again later.';
+    }
+    throw new Error(error);
+  }
+
+  const stream = response.body;
+  return stream;
+};
